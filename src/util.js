6286b75c8a807561bd5fbb7046198d40fa75a323
Renan Vaz <renanp.vaz@outlook.com>
2015-10-24 01:54:32 -0200

    feat(util.js): adicionar funções úteis

---
+++ src/util.js
@@ -0,0 +1,43 @@
+String.prototype.substituirPorUnderline = function(){
+return this.replace(/[^\s-]/g, '_').split('').join('');
+}
+
+//TODO: corrigir ultima posição
+
+Array.prototype.substituirPosicaoPorLetra = function(palavra, letra){
+  var self = this;
+  var last = palavra[palavra.lenght-1];
+  if(palavra.includes(letra)){
+    self.forEach(function(elem){
+      self.splice(palavra.indexOf(letra), 1, letra);
+      palavra = palavra.replace(letra, '_');
+      });
+      return self;
+    }
+  }
+
+  function pegaNumeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function pegarPalavraAleatoria(dificuldade){
+
+    var urlPalavras;
+    var palavraAleatoria;
+
+    if(dificuldade === 0){
+      urlPalavras = 'http://localhost:3000/0';
+    }else{
+      urlPalavras = 'http://localhost:3000/1';
+    }
+
+    //TODO: retornar palavraAleatoria invés de usar .append()
+
+    $.get(urlPalavras).done(
+      function(palavras){
+        var random = pegaNumeroAleatorio(0, palavras.length);
+        palavraAleatoria = palavras[random];
+        $('ul').append($('<li>').html(palavraAleatoria));
+    })
+
+  }
