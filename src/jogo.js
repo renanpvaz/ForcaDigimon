40031bbb2d357ea0ad176f54ab82053c011a3920
Felipe Ribeiro De Moraes <felipemoraes920@gmail.com>
2015-10-25 00:52:50 -0300

    fix(TelaInicial) agora é possivel iniciar jogo com 1 click

--- src/jogo.js
+++ src/jogo.js
@@ -1,13 +1,13 @@
 function Jogo(nomeJogador, dificuldade){
   atribuirPalavraAleatoria (dificuldade);
-  this.palavra = document.getElementById("palavra").innerHTML;
+  this.palavra = $('#palavra').text();
   console.log(this.palavra);
   this.espacosPalavra = substituirPorUnderline(this.palavra);
   //console.log(espacosPalavra);
   this.countErros = 0;
   this.nomeJogador = nomeJogador;
   this.dificuldade = dificuldade;
-  document.getElementById("mostraPalavra").innerHTML = this.espacosPalavra;
+  $('#mostraPalavra').text(this.espacosPalavra);
 }
 
 //var jogo = new Jogo($('#Nome').val(), 0 || 1 )
@@ -20,7 +20,7 @@ Jogo.prototype.incrementarErros = function(){
 Jogo.prototype.pegarPalavra = function(){
   pegarPalavraAleatoria(this.dificuldade, this)
 }
-/*
+
 Jogo.prototype.atribuirPalavraAleatoria = function(){
   var self = this;
     buscarPalavra(self.dificuldade).done(
@@ -32,12 +32,11 @@ Jogo.prototype.atribuirPalavraAleatoria = function(){
           self.espacosPalavra = jogo.palavra.substituirPorUnderline()
         });
 }
-*/
+
 function atribuirPalavraAleatoria (dificuldade){
     buscarPalavra(dificuldade).done(
       function(response){
       var palavra = response[Math.floor(Math.random() * (4 - 0 + 1)) + 0];
-      document.getElementById("palavra").style.visibility = "hidden";
-        document.getElementById("palavra").innerHTML = palavra;
+    $("#palavra").text(palavra);
      });
 };
